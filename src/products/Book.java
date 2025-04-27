82c4ed07c2648d4a579819f313235de47d8ae874
Mobin <Mobinmojiri1234@gmail.com>
2025-04-27 20:51:15 +0330

    coding feilds

--- src/products/Book.java
+++ src/products/Book.java
@@ -1,6 +1,13 @@
 package products;
 
 public class Book extends Product{
+
+
+    private String author;
+    private String publication;
+    private String genre;
+
+
     @Override
     protected String generateId() {
         return "";
