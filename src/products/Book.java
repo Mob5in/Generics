c824048d6242b6b5e4acbc999c8a71c849f8e10a
Mobin <Mobinmojiri1234@gmail.com>
2025-04-27 20:57:50 +0330

    coding toString for book

--- src/products/Book.java
+++ src/products/Book.java
@@ -8,6 +8,10 @@ public class Book extends Product{
     private String genre;
 
 
+    public String toString(){
+        return super.toString() + ", Author: " + author + ", Publication: " + publication + ", Genre: " + genre;
+    }
+
     @Override
     protected String generateId() {
         return "";
