1adf056a38ee151b1eec77a06aee32671cd84cb2
Mobin <Mobinmojiri1234@gmail.com>
2025-04-28 23:16:36 +0330

    coding constructor for notebook

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -1,10 +1,19 @@
 package products;
 
-public class Notebook {
+public class Notebook extends Product{
 
     private int pageCount;
     private boolean isHardCover;
 
 
+    public Notebook(String title, Double price, int pageCount, boolean isHardCover) {
+        super(title, price);
+        this.pageCount = pageCount;
+        this.isHardCover = isHardCover;
+    }
 
+    @Override
+    protected String generateId() {
+        return "";
+    }
 }
