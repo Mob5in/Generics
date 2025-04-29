c34c9633b09a3ca0a46fb30da98514642df8a9b9
Mobin <Mobinmojiri1234@gmail.com>
2025-04-29 20:00:26 +0330

    coding accessory generateId

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -2,15 +2,12 @@ package products;
 
 public class Accessory extends Product{
 
+    private static int counter = 0;
     private String color;
 
     public Accessory(String title, Double price) {
         super(title, price);
-    }
-
-    @Override
-    protected String generateId() {
-        return "";
+        counter ++;
     }
 
     public String getColor() {
@@ -26,5 +23,11 @@ public class Accessory extends Product{
         return super.toString() + "_ Color: " + color;
     }
 
+
+    @Override
+    protected String generateId() {
+        int count = counter;
+        return "31" + String.format("%02d", count);
+    }
 
 }
