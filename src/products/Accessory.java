904cab92ced23c9ae30ab09ae214bc0f8d709901
Mobin <Mobinmojiri1234@gmail.com>
2025-04-29 20:09:28 +0330

    coding Accessory set Id

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -8,6 +8,7 @@ public class Accessory extends Product{
     public Accessory(String title, Double price) {
         super(title, price);
         counter ++;
+        setId(generateId());
     }
 
     public String getColor() {
